8b5a506dae99cbe5822d765a25194cfcf97a9b05
Daniel Beebe <danwbeebe@gmail.com>
2017-09-07 13:35:20 -0400

    add bands to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-GoodBand.create(name: 'Nirvana', song: 'All Apologies', album: "Unplugged")
+GoodBand.create(name: 'Nirvana', song: 'All Apologies', album: 'Unplugged')
+GoodBand.create(name: 'Pearl Jam', song: 'Better Man', album: 'Vitalogy')
+GoodBand.create(name: 'Green Day', song: 'When I Come Around', album: 'Dookie')
 
-BadBand.create(name: 'Hanson', song: 'MMMBop', album: "Middle of Nowhere")
-
+BadBand.create(name: 'Hanson', song: 'MMMBop', album: 'Middle of Nowhere')
+BadBand.create(name: 'Hootie and the Blowfish', song: 'Only Wanna Be With You', album: 'Cracked Rear View')
+BadBand.create(name: 'NSYNC', song: 'Bye Bye Bye', album: 'No Strings Attached')
