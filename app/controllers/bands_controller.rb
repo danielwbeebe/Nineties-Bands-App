f37d1c9216048e6a761067d0b945d13e199ef378
Daniel Beebe <danwbeebe@gmail.com>
2017-09-07 18:36:05 -0400

    fix error with good bands form

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -5,4 +5,28 @@ class BandsController < ApplicationController
     @badbands = BadBand.all
   end
 
+  def create
+    goodband = params['goodband']
+    GoodBand.create(name: goodband['name'],
+                song: goodband['song'],
+                album: goodband['album'],
+                )
+    redirect_to root_path
+  end
+
+  def destroy
+    GoodBand.destroy(params['id'])
+    redirect_to root_path
+  end
+
+  def update
+    goodband = params['goodband']
+    GoodBand.update(name: goodband['name'],
+                song: goodband['song'],
+                album: goodband['album'],
+                )
+
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'bands#index'
+
+  # Credit and thanks Dov and Taka for showing need for this route
+  resources :bands
   resources :goodbands
   resources :badbands
 end
