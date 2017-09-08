1f9e3c57b88cfd88bc86c70523af35e405239e83
Daniel Beebe <danwbeebe@gmail.com>
2017-09-08 09:31:54 -0400

    fix error in create method for good and bad bands

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -5,22 +5,23 @@ class BandsController < ApplicationController
     @badbands = BadBand.all
   end
 
+# Credit and thanks to Ramsey for explaining if elsif usage inside the create method
   def create
-    goodband = params['goodband']
-    GoodBand.create(name: goodband['name'],
-                song: goodband['song'],
-                album: goodband['album'],
-                )
-    redirect_to root_path
-  end
+    if params['goodband']
+      goodband = params['goodband']
+      GoodBand.create(name: goodband['name'],
+                  song: goodband['song'],
+                  album: goodband['album'],
+                  )
+      redirect_to root_path
 
-  def create
-    badband = params['badband']
-    BadBand.create(name: badband['name'],
-                song: badband['song'],
-                album: badband['album'],
-                )
-    redirect_to root_path
+    elsif params['badband']
+      badband = params['badband']
+      BadBand.create(name: badband['name'],
+                  song: badband['song'],
+                  album: badband['album'],
+                  )
+      redirect_to root_path
+    end
   end
-
 end
